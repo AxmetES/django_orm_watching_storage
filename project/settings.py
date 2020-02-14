c7b8c984283366b9bb731a5b93120b58f6dd2fc6
AxmetES <frakstia@gmail.com>
2020-02-14 23:18:20 +0600

    -Add .env file. -Changed settings.py file , all secret parametrs are hidden.

--- project/settings.py
+++ project/settings.py
@@ -1,19 +1,22 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': 5434,
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
